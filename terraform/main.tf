c3f49a875492c19553ca1e23ccd2a0f5921f3507
Nikita Vetokhin <nmazarov97@gmail.com>
2026-04-01 13:16:04 +0300

    Add Terraform configuration for Kind cluster

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    kind = {
+      source  = "tehcyx/kind"
+      version = "0.2.0"
+    }
+  }
+}
+
+provider "kind" {}
+
+resource "kind_cluster" "default" {
+  name            = "devops-lab-cluster"
+  node_image      = "kindest/node:v1.27.3"
+  wait_for_ready  = true
+
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+    }
+    node {
+      role = "worker"
+    }
+  }
+}
